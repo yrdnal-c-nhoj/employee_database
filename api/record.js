c998ed6f92e7dfdd42a62d05f96b04e100553481
john-c-landry <johnclandry@yahoo.com>
2026-02-25 11:16:21 -0500

    Fix API to use CommonJS module.exports for Vercel

--- api/record.js
+++ api/record.js
@@ -23,7 +23,7 @@ async function connectToDatabase() {
   return { client, db };
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
