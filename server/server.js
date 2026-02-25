0d740412471f2da37f97b421ba1b06753a222436
john-c-landry <johnclandry@yahoo.com>
2026-02-25 11:27:08 -0500

    Complete removal of all Vercel references from project

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5050;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://employee-database-mu.vercel.app'],
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
